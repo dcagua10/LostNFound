e1fd5762591150ac7f1d0e32b43969206444aa5e
Daniel Cagua Ennis <d.cagua10@uniandes.edu.co>
2018-09-16 11:43:25 -0500

    Updates Visuales
    No entiendo porque el footer no se muestra

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,42 +2,60 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-
+  
   constructor(props){
     super(props);
-
+    
     this.state = {
       objetos : [
         {id:1 ,foundby_first_name:'Sapo', description:'No sirve', place:'SD'}
       ]
     };
   }
-
-  componentDidMount(){
-    fetch('/getData')
-      .then((res) => {
-        return res.json();
-      })
-      .then((json) => this.setState({objetos:json}))
-      .catch((err) => console.log(err));
-  }
-
-  renderObjects() {
-    return this.state.objetos.map((obj) => 
-    // <Objeto objeto={obj}/>
-      <div key={obj.id}>{obj.foundby_first_name} - {obj.description} in {obj.place}</div>
+  
+  renderTopObjects() {
+    return ( 
+      // <Objeto objeto={obj}/>
+      //<div key={obj.id}>{obj.foundby_first_name} - {obj.description} in {obj.place}</div>
+      <nav class="navbar navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+      <div class="navbar-expand" id="navbar">
+      <div class="navbar-nav">
+      <a class="navbar-brand" href="#">Welcome to LostNFound</a>
+    </div>
+    </div>
+    <div class="dividerRight"/>
+    <button class="btn btn-outline-info my-2 my-sm-0 pull-right" onclick="changeColor()">Sign In</button>
+    <div class="divider"/>
+    <button class="btn btn-outline-info my-2 my-sm-0 pull-right" onclick="changeColor()">Sign Up</button>
+    </nav>
     );
   }
-
+  
+  renderContent()
+  {
+    <div className="content">
+    {this.props.children}
+    </div>
+  }
+  
+  renderLowObjects()
+  {
+    <footer class="page-footer font-small blue">
+    <div class="footer-copyright text-center py-3">© 2018 Copyright: Orlando Sabogal and Daniel Cagua
+    </div>
+    </footer>
+  }
+  
   render() {
     return (
       <div className="App">
-        <h1>Welcome to LostNFound</h1>
-        {this.renderObjects()}
+      {this.renderTopObjects()}
+      {this.renderContent()}
+      {this.renderLowObjects()}
       </div>
-    );
+      );
+    }
   }
-}
   
-export default App;
+  export default App;
   
