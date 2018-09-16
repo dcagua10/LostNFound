aa7e8a1ad50940c84313ba22dc13dc0d7d974a05
Daniel Cagua Ennis <d.cagua10@uniandes.edu.co>
2018-09-16 10:50:18 -0500

    otro update miseable

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -32,7 +32,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Lost N Found</h1>
+        <h1>Welcome to LostNFound</h1>
         {this.renderObjects()}
       </div>
     );
