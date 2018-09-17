3c288b9f3516f130b5e51404d1fd85761b2a5a82
osabogal10 <o.sabogal10@uniandes.edu.co>
2018-09-17 02:07:18 -0500

    ...

--- app.js
+++ app.js
@@ -20,6 +20,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'frontend/build')));
+// Anything that doesn't match the above, send back index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/frontend/build/index.html'))
+});
 
 app.use('/', indexRouter);
 app.use('/',siginrouter);
